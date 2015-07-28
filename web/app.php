3bb86ff77a51335bcc4ad8a6b174b2f9b7a3b5b4
Jess Sevilleja <jmvsevilleja@gmail.com>
2015-07-28 20:05:10 +0800

    another commit

--- web/app.php
+++ web/app.php
@@ -17,8 +17,8 @@ $apcLoader->register(true);
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
-die('x');
-$kernel = new AppKernel('prod', true);
+
+$kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
